6b05b4909c3504d104c18f415eeac0b54ce7633e
Henrique David de Medeiros <henriquemed101@gmail.com>
2018-05-26 15:20:54 -0300

    Adição de objetos.

--- include/game.h
+++ include/game.h
@@ -2,22 +2,31 @@ class SnazeGame{
 
 private:
 	std::vector<Maps> mapas;
+	Snaze cobrinha;
+	
+
 
 public:
 
-
+	// Para fins de compilação.
 	void print_all_maps(){
 		std::cout << "MAPA = " << mapas[0].return_x() << " " << mapas[0].return_y() << std::endl;
 		for( auto & i : mapas ){
 			i.printVector();
 		}
+		std::pair<int,int> pos = mapas[0].snaze_position();
+		std::cout << "A cobra está na posição: (" << pos.first << "," << pos.second << ")" << std::endl;
 
 	}
-
+	/// Inicializa o jogo.
 	void initialize_game(std::string inputdata){
 
 		this->mapas = readMaps(inputdata);
 		print_all_maps();
+
+		cobrinha.set_position(mapas[0].snaze_position());
+		std::pair<int,int> pos = cobrinha.get_position();
+		std::cout << "A cobra está na posição: (" << pos.first << "," << pos.second << ")" << std::endl;
 
 	}
 
